6740546ca8b0cffc56f3fa74457e20d4b14db4c7
Chris <149903844+DeveloperChrisP@users.noreply.github.com>
2025-01-14 00:05:07 +0000

    Add files via upload

--- hymnsAccordion.js
+++ hymnsAccordion.js
@@ -18,78 +18,111 @@ function Hymn(title,hymnNumber,sheetMusic,vocalPart,video){
 allLitergies = [];
 
 
-function Litergy (date,occasion,year,hymn){
+function Litergy (date,occasion,year,selector,hymn){
     this.date = date;
     this.occasion = occasion;
     this.year = year;
+    this.selector = selector;
     this.hymn = hymn;
 
     date.setHours(10);date.setMinutes(30);
     allLitergies.push(this);
 } 
 
+new Hymn ("Alleluia, sing to Jesus",644,"","","https://www.youtube.com/embed/UJDWFYIkBns?si=FjlGK0j-FEm0vRXU");
+new Hymn ("Amazing Grace",846,"","","https://www.youtube.com/embed/HsCp5LG_zNE?si=SVRmh5evJYOhTVy8");
 new Hymn ("As I kneel before you",360,"","","https://www.youtube.com/embed/BYv6E9xnJ1U?si=W5qvVIl1dVdmlVEA");
 new Hymn ("As with gladness men of old",166,"","","https://www.youtube.com/embed/y-TplMYH0Ko?si=abx_LTDZfoet_c44");
 new Hymn ("Bethlehem of noblest cities",167,"","","https://www.youtube.com/embed/rlbmmn9uOLg?si=H0LS3ygSKcTXnlA6")
-new Hymn ("Blessed virgin mother",361,"","","")
+new Hymn ("Blessed virgin mother",361,"","[Piano]","");
+new Hymn ("Blest are you, Lord",603,"","","https://www.youtube.com/embed/tsGr49LpuGI?si=p4yTl1Xy_chXtTgN");
 new Hymn ("Bread of life",631,"","","https://www.youtube.com/embed/iYKxMAn3Kfo?si=Fei0At7L0HqudqQ3");
+new Hymn ("Daily,daily sing to Mary",353,"","","https://www.youtube.com/embed/Xwkca-3GiM4?si=UE2_gA3FfHCirCC0");
+new Hymn ("Dear Lord and Father of mankind",934,"","","https://www.youtube.com/embed/b1MN3chW1Hk?si=C30Yg9l0jHnjT-HO");
+new Hymn ("Follow me, follow me",863,"","","https://www.youtube.com/embed/fzkP3jy8BEM?si=f4M8NvoxXceQJcbr");
 new Hymn ("Hail Queen of heaven",356,"","","https://www.youtube.com/embed/xfkPTmry3zI?si=SkUYW9G4hlpippRf");
 new Hymn ("Hark the Herald Angels Sing",155,"","","https://www.youtube.com/embed/9Bwn0k0k8xI?si=vilS4HDunUKoLrlC");
 new Hymn ("Holy is his name",339,"","","https://www.youtube.com/embed/uILEUS4fyQg?si=mMs3PRADRaNuBI2s")
 new Hymn ("Holy virgin by God's decree",366,"","","https://www.youtube.com/embed/aJZhYBXse2g?si=j7DLVkqiusbcid97");
 new Hymn ("I heard the voice of Jesus say",795,"","","https://www.youtube.com/embed/Kh4XxlWZhMs?si=xmfUNd4cYzk_fRjQ");
 new Hymn ("I'll sing a hymn to Mary",355, "","","https://www.youtube.com/embed/SB3Rc2gXGH8?si=-pVYf2yLsbBscT0e");
+new Hymn ("Immaculate Mary",365,"","","https://www.youtube.com/embed/hXqTGl6mbYg?si=62ZRgf9GcBlBYmM0");
 new Hymn ("In the bleak mid-winter",144,"","","https://www.youtube.com/embed/cBCYZ9jIJkI?si=7IfiGM2MqBrA1q6t");
 new Hymn ("In the Breaking of the Bread",624,"","","https://www.youtube.com/embed/0H8CPadsVco?si=TEfbe6JW7aAJ_ZVA");
 new Hymn ("It came upon the midnight clear",135,"","","https://www.youtube.com/embed/hx7LJZIXXJo?si=7a3g6ieIB0AZB4MY");
+new Hymn ("Lay your hands",432, "","","https://www.youtube.com/embed/q5UvY7itnn8?si=tRTETqvYEvcjjvSg");
 new Hymn ("Long ago prophets knew",116,"","","https://www.youtube.com/embed/OeLA1LhqFA4?si=Y_jd5YhNoRzLi9ae");
 new Hymn ("O Come all ye faithful",159,"","","https://www.youtube.com/embed/8ZYZEr3JtZY?si=ZRXVkFmw8cKqnLO1");
 new Hymn ("O let all who thirst",408,"","","https://www.youtube.com/embed/MwG6PCWnih0?si=qltwXx5awggq5kJp");    
 new Hymn ("O Mary Conceived In The Grace Of Your Son",354,"",["Piano"],"");
 new Hymn ("On Christmas night all Christians sing",134,"","","https://www.youtube.com/embed/wxaLyJ5G4zM?si=ntSZgM5_fK_rTuF-");
 new Hymn ("On Jordan's bank",94,"","","https://www.youtube.com/embed/OVHkMWwnGWk?si=jIJKo6aQAzqYlVFL")
+new Hymn ("One Bread, One Body",832,"","","https://www.youtube.com/embed/WHAu3fLF7NI?si=Dfu0x8PsljIxRoY0");
 new Hymn ("Once in royal David's city",128,"","","https://www.youtube.com/embed/PtiCLJvyePw?si=TeMsFVrdT232gYs2");
+new Hymn ("Praise my soul the King of Heaven",807,"","","https://www.youtube.com/embed/GyRIVbdsi4c?si=ittnj3ufy9aj98Nj");
 new Hymn ("Praise to you o christ our saviour",200,"","","https://www.youtube.com/embed/mybxBzhONyU?si=TAS1uxRc6ZY-Bq5V");
 new Hymn ("Silent Night",136,"./sheetMusic/SilentNight.pdf",["satb", "melody","alto","tenor","bass","piano"],"https://www.youtube-nocookie.com/embed/POcDlbYiF9c?si=cBj0zCzSgsO6fK7T");
-new Hymn ("Sing of Mary",341,"","","https://www.youtube.com/embed/Ap6Hym_ERQM?si=dN6iC0-7Wsk2DXnz");
+new Hymn ("Sing of Mary, pure and lowly",341,"","","https://www.youtube.com/embed/Ap6Hym_ERQM?si=dN6iC0-7Wsk2DXnz");
+new Hymn ("Sing of the Lord's Goodness",713,"","","https://www.youtube.com/embed/xzTXl_jA-XQ?si=4Poy3GZQvRYyieDe")
+new Hymn ("Sing we of the blessed Mother",344,"","","https://www.youtube.com/embed/zYZ4MbY5kG0?si=2NTmZecGpeCPOkvP");
 new Hymn ("The angel Gabriel",113,"","","https://www.youtube.com/embed/pliqObTHxUQ?si=n-iBUx5I6ggQvds1");
+new Hymn ("The Church's One Foundation",830,"","","https://www.youtube.com/embed/1ooRvCyULoU?si=c381yxAE3XBSKWhi");
 new Hymn ("The first Noel",150,"","","https://www.youtube.com/embed/1mItWsC8RtM?si=_cbhrtdvUladP4Yn");
-new Hymn ("The king of glory comes",107,"","","https://www.youtube.com/embed/aTl1Q8yudfM?si=x0GFpzoPCkMWtZRA")
+new Hymn ("The king of glory comes",107,"","","https://www.youtube.com/embed/aTl1Q8yudfM?si=x0GFpzoPCkMWtZRA");
+new Hymn ("The light of Christ",747,"","","https://www.youtube.com/embed/ylgeOR1LT_s?si=_01aQwXMVrFL7dOY");
+new Hymn ("The Summons",877,"","","https://www.youtube.com/embed/VFXAmB_Iq7k?si=6VEiE0TBf4zx_8W0");
 new Hymn ("The voice of God",108,"","","https://www.youtube.com/embed/r9EohwMRurc?si=35hV3H_r-sr145bJ")
 new Hymn ("This is my body",627,"","","https://www.youtube.com/embed/O-60hfhJzqI?si=_vXX2hQT8MWV5zwl")
 new Hymn ("Unto us is born a son",152,"","","https://www.youtube.com/embed/4_4xMMpcqbM?si=RhsMJeDjUpuP2bYO");
 new Hymn ("Water of life",512,"","","https://www.youtube.com/embed/p153CSsGRxs?si=_yModMrXNcTu4vYm");
+new Hymn ("We have a gospel to proclaim",852,"","","https://www.youtube.com/embed/OXlojQCipg4?si=fxCJvBdV-nCFq-It");
 new Hymn ("We three kings",170,"","","https://www.youtube.com/embed/Lx35_DRIZ8g?si=bCTGGMdpULJm0UDc");
 new Hymn ("What child is this",145,"","","https://www.youtube.com/embed/6jroBAl3WW8?si=kZEj9G4qmknU1RlB");
-
+new Hymn ("Where are you bound, Mary?",343,"","","https://www.youtube.com/embed/rPWOEZzNhxM?si=w_alalLvviuN7AX5");
 
 //specify litergys
-new Litergy (new Date("08 dec 2024"), "2nd Sunday Advent","C",
+new Litergy (new Date("08 dec 2024"), "2nd Sunday Advent","C","Chris",
 grabHymnObject("On jordan's bank","praise to you o christ our saviour","this is my body","holy virgin by god's decree"))
 
-new Litergy (new Date("15 dec 2024"), "3rd Sunday Advent","C",
+new Litergy (new Date("15 dec 2024"), "3rd Sunday Advent","C","Chris",
 grabHymnObject("the voice of god","holy is his name", "the king of glory comes","blessed virgin mother"));
 
-new Litergy (new Date("22 dec 2024"), "4th Sunday Advent","C",
+new Litergy (new Date("22 dec 2024"), "4th Sunday Advent","C","Chris",
 grabHymnObject("long ago prophets knew","the angel gabriel", "in the breaking of the bread","o mary conceived in the grace of your son"));
 
-new Litergy (new Date("24 dec 2024"), "Christmas Eve","C",
+new Litergy (new Date("24 dec 2024"), "Christmas Eve","C","Chris",
 grabHymnObject("on christmas night all christians sing","it came upon the midnight clear", "once in royal david's city","the first noel"));
 
-new Litergy (new Date("25 dec 2024"), "Christmas Day","C",
+new Litergy (new Date("25 dec 2024"), "Christmas Day","C","Chris",
 grabHymnObject("unto us is born a son","hark the herald angels sing","silent night","o come all ye faithful"));
 
-new Litergy (new Date("29 dec 2024"), "Feast of Holy Family","C",
-grabHymnObject("what child is this","in the bleak mid-winter","bread of life","sing of mary"));
+new Litergy (new Date("29 dec 2024"), "Feast of Holy Family","C","Chris",
+grabHymnObject("what child is this","in the bleak mid-winter","bread of life","sing of mary, pure and lowly"));
 
-new Litergy (new Date("01 jan 2025"), "Mary the Holy mother of God","C",
+new Litergy (new Date("01 jan 2025"), "Mary the Holy mother of God","C","Chris",
 grabHymnObject("I'll sing a hymn to mary","hail queen of heaven","as i kneel before you","holy virgin by god's decree"));
 
-new Litergy (new Date("5 jan 2025"), "The Epiphany","C",
+new Litergy (new Date("5 jan 2025"), "The Epiphany","C","Chris",
 grabHymnObject("bethlehem of noblest cities","As with gladness men of old","We three kings","As I kneel before you"));
 
-new Litergy (new Date("12 jan 2025"), "The Baptism of the Lord","C",
+new Litergy (new Date("12 jan 2025"), "The Baptism of the Lord","C","Chris",
 grabHymnObject("i heard the voice of Jesus say","water of life","o let all who thirst","hail queen of heaven"));
+
+new Litergy (new Date("19 jan 2025"), "2nd Sunday of Ordinary Time","C","Chris",
+grabHymnObject("The Church's one foundation","Lay your hands","One Bread, One Body","Immaculate Mary"));
+
+new Litergy (new Date("26 jan 2025"), "3rd Sunday of Ordinary Time","C","Chris",
+grabHymnObject("We have a gospel to proclaim","Blest are you, Lord","This is my body","Where are you bound, Mary?"));
+
+new Litergy (new Date("2 Feb 2025"), "The Presentation of the Lord","C","Chris",
+grabHymnObject("Sing of the Lord's Goodness","The King of Glory Comes","The light of Christ","Daily,daily sing to Mary"));
+
+new Litergy (new Date("9 Feb 2025"), "5th Sunday of Ordinary Time","C","Chris",
+grabHymnObject("Follow me, follow me","The Summons","Dear Lord and Father of mankind","Sing we of the blessed Mother"));
+
+new Litergy (new Date("16 Feb 2025"), "6th Sunday of Ordinary Time","C","Chris",
+grabHymnObject("Praise my soul the King of Heaven","Alleluia, sing to Jesus","Amazing Grace","Sing of Mary, pure and lowly"));
+
 
 allLitergies.sort(function(a,b){return a.date - b.date}); //sort Liturgy array incase not sorted manually
 
@@ -153,6 +186,7 @@ function ordinal (day){ //add 'st','nd','rd' or 'th' to date
                  break;
              
              default:
+                nth = "th"
                  break;
          }
      if (day >= 4 && day <= 20){
