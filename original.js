ee415aaa2a36ee865ce68fc40c5fdef9c8a9da4a
Chris <developer.chris@hotmail.com>
2026-08-30 12:26:06 +0100

    add 23rd yrA Psalm

--- original.js
+++ original.js
@@ -174,6 +174,7 @@ new Psalm("119(118)", "57, 72. 76-77. 127-128. 129-130. ℟97a", "O Lord, how I 
 new Psalm("145(144)v2", "8-9. 15-16. 17-18. ℟ cf. 16", "You open your hand and fulfil us, O Lord.", "./sheetMusic/psalms/Psalm 145(144)v2/Psalm 145(144)v2.pdf", "./sheetMusic/psalms/Psalm 145(144)v2/Psalm 145(144)v2.jpg", { "SATB": "./audio/psalms/Psalm 145(144)v2/Psalm 145(144)v2 - SATB.mp3", "Melody": "./audio/psalms/Psalm 145(144)v2/Psalm 145(144)v2 - Melody.mp3", "Alto": "./audio/psalms/Psalm 145(144)v2/Psalm 145(144)v2 - Alto.mp3", "Tenor": "./audio/psalms/Psalm 145(144)v2/Psalm 145(144)v2 - Tenor.mp3", "Bass": "./audio/psalms/Psalm 145(144)v2/Psalm 145(144)v2 - Bass.mp3" })
 new Psalm("45(44)", "10. 11. 12. 16. ℟10b", "On your right stands the queen in gold of Ophir")
 new Psalm("138(137)", "1-2a. 2bc, 3. 6, 8bc. ℟8bc", "O Lord, your merciful love is eternal; discard not the work of your hands.")
+new Psalm("95(94)", "1-2. 6-7b. 7c-9. ℟7c, 8a", "O that today you would listen to his voice! ‘Harden not your hearts.’", "./sheetMusic/psalms/Psalm 95(94)/Psalm 95(94) - Piano.pdf", "./sheetMusic/psalms/Psalm 95(94)/Psalm 95(94) - SATB.jpg")
 
 new AlleluiaVerse("John 14:18", "I will not leave you as orphans, says the Lord.", "I am going away and I will come to you, and your hearts will rejoice.")
 new AlleluiaVerse("unknown", "Come, Holy Spirit, fill the hearts of your faithful", "and kindle in them the fire of your love.")
@@ -580,8 +581,8 @@ new Liturgy(new Date("9 Aug 2026"), "19th Sunday in Ordinary Time", "A", "Chris"
 new Liturgy(new Date("16 Aug 2026"), "The Assumption of the Blessed Virgin Mary", "A", "Chris", ["Holy is his name", "My God loves me", "Mary immaculate, star of the morning", "Immaculate Mary"], ["Magnificat"], "45(44)")
 new Liturgy(new Date("23 Aug 2026"), "21st Sunday in Ordinary Time", "A", "Chris", ["All my hope on God is founded", "Praise to the Lord, the Almighty", "Be still for the presence of the Lord", "Holy Virgin, by God's decree"], ["Out of Darkness", "Bread of life", "The summons"], "138(137)")
 new Liturgy(new Date("30 Aug 2026"), "22nd Sunday in Ordinary Time", "A", "Chris", ["Dear Lord and father of mankind", "O God you search me", "Christ be our light", "O lady full of God's own grace"], ["Brother sister let me serve you", "Out of darkness", "The summons", "Unless a grain of wheat"])
-new Liturgy(new Date("6 Sept 2026"), "23rd Sunday in Ordinary Time", "A", "Chris", ["Gather us in", "Brother, sister let me serve you", "This is my body", "Sing of Mary, pure and lowly"], ["Bread of life"])
-new Liturgy(new Date("13 Sept 2026"), "24th Sunday in Ordinary Time", "A", "Chris", ["All my hope on God is founded", "freely freely", "O let all who thirst", "As I kneel before you"], ["Taste and see", "ubi caritas", "My God loves me"])
+new Liturgy(new Date("6 Sept 2026"), "23rd Sunday in Ordinary Time", "A", "Chris", ["Gather us in", "Brother, sister let me serve you", "This is my body", "Sing of Mary, pure and lowly"], ["Bread of life"], "95(94)")
+new Liturgy(new Date("13 Sept 2026"), "24th Sunday in Ordinary Time", "A", "Chris", ["freely freely", "Make me a channel of your peace", "O let all who thirst", "As I kneel before you"], ["Taste and see", "ubi caritas", "My God loves me"])
 //2nd/ 6th/ 17th /20th (A) Sunday of ordinary - 26th of July: "Teach Me, O God (Christopher Walker)"
 
 let nextLiturgy = allLiturgies.filter(x => x.date >= todaysDate);
